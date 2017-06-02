64d3f38b934ff06ed6bd15cc3bb50d5120980e88
rygrtt <ryangrotte@gmail.com>
2017-06-02 13:51:01 -0500

    Delete common.js

--- GroupProject/WebContent/WEB-INF/javascript/common.js
+++
@@ -1,8 +0,0 @@
-function addListeners(){
-	var submitBtn = document.getElementById("searchBtn");
-	if (submitBtn.addEventListener) { // google/FireFox
-		submitBtn.addEventListener('click',validate, false);
-	} else if(submitBtn.attachEvent){ //IE or edge
-		submitBtn.attachEvent('click',validate);
-	}
-}
